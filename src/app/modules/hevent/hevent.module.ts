555195f92ea3b32340203ec031c20e4fba987c58
Jr-Oliveira-Silva <jr.oliveira.dev@gmail.com>
2024-09-15 13:20:00 -0300

    :white_check_mark: Starting hevent entity modeling

--- src/app/modules/hevent/hevent.module.ts
+++ src/app/modules/hevent/hevent.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { HeventService } from './hevent.service';
 import { HeventController } from './hevent.controller';
+import { CommonModule } from '../common/common.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hevent } from './entities/hevent.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Hevent]),
+    CommonModule
+  ],
   controllers: [HeventController],
   providers: [HeventService],
 })
-export class HeventModule {}
+export class HeventModule { }
