8eed46272995895984a56fcba8f7b75b607143fc
Jr-Oliveira-Silva <jr.oliveira.dev@gmail.com>
2024-09-16 20:27:26 -0300

    :wrench: Modifying pipeline for diferent environments

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = Number.parseInt(process.env["APPLICATION_PORT"]);
-  const env = process.env["NODE_ENV"];
-  if (env == "development") {
+
+  dotenv.config();
+  
+  const devMode = process.env.DEV_MODE ?? false;
+
+  if (devMode) {
+    const port = Number.parseInt(process.env.APPLICATION_PORT);
+    const env = process.env.NODE_ENV;
+
     const config = new DocumentBuilder()
       .setTitle('Happye API')
       .setVersion('1.0')
@@ -14,11 +21,19 @@ async function bootstrap() {
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('swagger', app, document);
+
+    await app.listen(port).then((value) => {
+      console.log(`Application running in port : ${port}, enviroment: ${env}`);
+    });
+
+  } else {
+    dotenv.config({ path: process.cwd() + '/.env.prod' });
+    const port = Number.parseInt(process.env.APPLICATION_PORT);
+    const env = process.env.NODE_ENV;
+
+    await app.listen(port).then((value) => {
+      console.log(`Application running in port : ${port}, enviroment: ${env}`);
+    });
   }
-
-  await app.listen(port).then((value) => {
-    console.log(`Application running in port : ${port}, enviroment: ${env}`);
-  });
-
 }
 bootstrap();
