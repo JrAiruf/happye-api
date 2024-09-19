3d9d0e90ed2ac3b2271528d60febed7b94ddc627
Jr-Oliveira-Silva <jr.oliveira.dev@gmail.com>
2024-09-18 21:04:03 -0300

    :hammer: Updating relations between User, Speecher and Hevent

--- src/app/modules/speecher/entities/speecher.entity.ts
+++ src/app/modules/speecher/entities/speecher.entity.ts
@@ -1,14 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, OneToOne } from 'typeorm';
 import { Hevent } from '../../hevent/entities/hevent.entity';
+import { User } from '../../user/entities/user.entity';
 
 @Entity('speechers')
 export class Speecher {
     @PrimaryGeneratedColumn('uuid')
     id: string
+
     @Column()
     resume?: string
+
     @Column()
     about?: string
+
     @ManyToMany(() => Hevent)
     events?: Hevent[]
+
+    @OneToOne(() => User)
+    user?: User
 }
