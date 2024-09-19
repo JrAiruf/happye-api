ba611beab02198b954452cba01bbb1f1afaa1b4e
Jr-Oliveira-Silva <jr.oliveira.dev@gmail.com>
2024-09-18 21:04:37 -0300

    :hammer: Setting User properties

--- src/app/modules/user/entities/user.entity.ts
+++ src/app/modules/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, ManyToMany } from 'typeorm';
+import { Speecher } from '../../speecher/entities/speecher.entity';
+
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({ length: 200 })
+    fullName: string
+
+    @Column({ length: 14 })
+    phoneNumber: string
+
+    @OneToOne(() => Speecher, { eager: true, cascade: true })
+    speecher?: Speecher
+}
